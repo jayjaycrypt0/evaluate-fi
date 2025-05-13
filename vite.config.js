70ca2864dfb5a805d89c5de7856216d3bc09a405
jayjay <jayjaycrypt0@protonmail.com>
2025-05-13 17:58:55 +0200

    Update configuration for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/evaluate-fi/',
-=======
   base: '/',
->>>>>>> 601177b979e8818b5e54ee1f57d9a3f79e645a69
   plugins: [react()]
 })
