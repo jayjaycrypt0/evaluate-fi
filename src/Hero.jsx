40407becb62185ad2fb58bea59b6a20e185c54cc
jayjay <jayjaycrypt0@protonmail.com>
2025-05-14 09:46:24 +0200

    Textual updates

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -51,7 +51,7 @@ export default function Hero() {
           <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-12 space-y-8">
             {/* Announcement Banner */}
             <div className="flex justify-center">
-              <a className="group inline-flex items-center bg-white/10 hover:bg-white/10 border border-white/10 p-1 ps-4 rounded-full shadow-md focus:outline-hidden focus:bg-white/10" href="../figma.html">
+              <a className="group inline-flex items-center bg-white/10 hover:bg-white/10 border border-white/10 p-1 ps-4 rounded-full shadow-md focus:outline-hidden focus:bg-white/10" href="/">
                 <p className="me-2 text-white text-sm">
                   Watch demo
                 </p>
@@ -65,14 +65,14 @@ export default function Hero() {
             {/* Title */}
             <div className="max-w-3xl text-center mx-auto">
               <h1 className="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-                The first fully autonomous logic agentic network.
+                The first fully autonomous logic agentic platform.
               </h1>
             </div>
             {/* End Title */}
 
             <div className="max-w-3xl text-center mx-auto">
               <p className="text-lg text-white/70">
-              Smarter trading starts here. Get AI-driven signals, technical analyisis, market intelligence, and deep social analytics for crypto and finance—all in one place.
+              AI-driven signals, technical analyisis, market intelligence, and deep social analytics for crypto and finance—all in one place.
               </p>
             </div>
 
